e93bed186cb7c2c7e29b2faff9ce342ac65ee39d
FeuFeve <clement-potin@outlook.com>
2020-01-26 21:36:36 +0100

    "Fixed" a bug that was causing the vertical layouts to display one above the others

--- Project Antz/Assets/Scripts/PageLoader.cs
+++ Project Antz/Assets/Scripts/PageLoader.cs
@@ -1,16 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class PageLoader : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> deactivatePagesInside;
     [SerializeField] private GameObject startingPage;
     public static GameObject currentPage;
 
+
     void Start()
     {
+        // Deactivate all pages of the different folders
+        foreach(GameObject pageFolder in deactivatePagesInside)
+        {
+            foreach(Transform page in pageFolder.transform)
+            {
+                page.gameObject.SetActive(false);
+            }
+        }
+
+        // Activate the Welcome Page if it's not already the case
         currentPage = startingPage;
         currentPage.SetActive(true);
     }
 
+    // SWITCH FROM A PAGE TO ANOTHER
     public void SwitchToPage(GameObject pageToLoad)
     {
         currentPage.SetActive(false);
